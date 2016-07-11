bfa1a282b7b17cb88c895c7a7bbfeaaec453e8c5
haredmah <haredmah@siu.edu>
2016-07-11 17:53:04 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## code for inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+inv <- NULL 
+set <- function(y) { 
+x <<- y
+inv <<- NULL
 
 }
 
 
-## Write a short comment describing this function
+## Computing the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        get <- function() x
+        
+        setinverse <- function (inverse) inv <<- inverse
+        
+        getinverse <- function() inv
+        
+        list ( set =set
+               get= get
+               setInverse = setInverse
+               getInverse = setInverse )
+        
+        
 }
